e6419a05fa3d65b43154eaf53f5a607b994fa571
Sinobom-Plaatjie <4270448@myuwc.ac.za>
2022-04-07 15:48:14 +0200

    Completed tic tac toe game

--- TicTacToeSubmissionSln/TicTacToeSubmissionSln/TicTacToeSubmissionConole/TicTacToe.cs
+++ TicTacToeSubmissionSln/TicTacToeSubmissionSln/TicTacToeSubmissionConole/TicTacToe.cs
@@ -9,22 +9,24 @@ namespace TicTacToeSubmissionConole
     public class TicTacToe
     {
         private TicTacToeConsoleRenderer _boardRenderer;
+        private int[] _boardPosition = new int[9];
+        private int _rounds;
+
 
         public TicTacToe()
         {
             _boardRenderer = new TicTacToeConsoleRenderer(10,6);
             _boardRenderer.Render();
         }
-
-
-        public void Run()
+        private void PlayMove(int player)
         {
-
-            // FOR ILLUSTRATION CHANGE TO YOUR OWN LOGIC TO DO TIC TAC TOE
-
             Console.SetCursorPosition(2, 19);
 
+            if(player == 1)
             Console.Write("Player X");
+             else
+                Console.Write("Player O");
+
 
             Console.SetCursorPosition(2, 20);
 
@@ -37,13 +39,103 @@ namespace TicTacToeSubmissionConole
             Console.Write("Please Enter Column: ");
             var column = Console.ReadLine();
 
+            Console.Clear();
 
+            //store move in array
+            int rowNumber = int.Parse(row);
+            int columnNumber = int.Parse(column);
+            int arrayPos = (rowNumber * 3) + columnNumber;
 
+            _boardPosition[arrayPos] = player;
 
-            // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
-            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);            
+            if (player == 1)
+                _boardRenderer.AddMove(rowNumber, columnNumber, PlayerEnum.X, true);
+            else
+                _boardRenderer.AddMove(rowNumber, columnNumber, PlayerEnum.O, true);
 
+            //Console.Clear();
         }
+        public bool CheckIfWins(int player)
+        {   //*Horizontal win
+            if ((_boardPosition[0] == player) && (_boardPosition[1] == player) && (_boardPosition[2] == player))
+               
+                return true;
+            //*diagonal win 
+            if ((_boardPosition[0] == player) && (_boardPosition[4] == player) && (_boardPosition[8] == player))
+
+                return true;
+
+            //*vertical win
+
+            if ((_boardPosition[0] == player) && (_boardPosition[3] == player) && (_boardPosition[6] == player))
+
+                return true;
+
+            //*
+            if ((_boardPosition[3] == player) && (_boardPosition[4] == player) && (_boardPosition[5] == player))
+
+                return true;
+            //*
+            if ((_boardPosition[6] == player) && (_boardPosition[7] == player) && (_boardPosition[8] == player))
+
+                return true;
+            //*
+            if ((_boardPosition[6] == player) && (_boardPosition[4] == player) && (_boardPosition[2] == player))
+
+                return true;
+
+            if ((_boardPosition[1] == player) && (_boardPosition[4] == player) && (_boardPosition[7] == player))
+
+                return true;
+
+            if ((_boardPosition[2] == player) && (_boardPosition[5] == player) && (_boardPosition[8] == player))
+
+                return true;
+
+
+            return false;
+        }
+   
+        public void Run()
+        {
+            _rounds = 0;
+            bool playerXWins = false;
+            bool playerOWins = false;
+
+            while (_rounds < 4)
+            {
+                PlayMove(1);
+                playerXWins = CheckIfWins(1);
+
+                if(playerXWins)
+                {
+                    Console.SetCursorPosition(22, 27);
+                    Console.WriteLine("Player X Win!!");
+                    break;
+                }
+              
+
+                PlayMove(2);
+                playerOWins = CheckIfWins(2);
+
+                if (playerOWins)
+                {
+                    Console.SetCursorPosition(22, 27);
+                    Console.WriteLine("Player O Win!!");
+                    break;
+                }
+              
+
+                _rounds++;
+            }
+            if (!playerXWins && !playerOWins)
+            {
+                Console.SetCursorPosition(22, 27);
+                Console.WriteLine("The game is draw!");
+
+            }
+        }
+        
 
     }
 }
